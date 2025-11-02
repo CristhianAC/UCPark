2c4c0064beb009e23a8f2d3764d44bd0f27fd888
Cristhian Agamez <cristhian_agamez@outlook.com>
2025-11-02 02:58:58 +0100

    Add IUserManagement interface methods and implement Card and Complaint classes

--- ucpark/ucpark_common/src/main/java/es/unican/ps/business/IUserManagement.java
+++ ucpark/ucpark_common/src/main/java/es/unican/ps/business/IUserManagement.java
@@ -1,5 +1,31 @@
 package es.unican.ps.business;
 
+import java.util.List;
+
+import es.unican.ps.entities.Card;
+import es.unican.ps.entities.Complaint;
+
 public interface IUserManagement {
+
+    /**
+     * Pays the complaints associated with a vehicle identified by its plate
+     * number using the provided card.
+     *
+     * @param card The card used for payment.
+     * @param plateNumber The plate number of the vehicle whose complaints are
+     * to be paid.
+     * @return true if the payment was successful, false otherwise.
+     */
+    public boolean payComplaints(Card card, String plateNumber);
+
+    /**
+     * Retrieves the list of complaints associated with a vehicle identified by
+     * its plate number.
+     *
+     * @param plateNumber The plate number of the vehicle whose complaints are
+     * to be retrieved.
+     * @return A list of complaints associated with the vehicle.
+     */
+    public List<Complaint> getComplaints(String emailUser);
 
 }
